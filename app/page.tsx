abc37e11b3193967645262f54654927dc9cfd2ed
Edward Morhe <edwardmorhe777@gmail.com>
2025-11-07 22:24:57 -0700

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
+import { ArrowUpNarrowWide, BarChart3, TrendingUp } from "lucide-react";
 import Image from "next/image"
 import Link from "next/link"
+
+interface HeaderProps {
+  id: number;
+  title: string;
+  subtitle: string;
+  icon: React.ComponentType<{ className?: string }>
+}
+
+const header: HeaderProps[] = [
+  {id:1, title:"+12k", subtitle:"Personalised  users", icon:TrendingUp},
+  {id:2, title:"70%", subtitle:"Productivity", icon:ArrowUpNarrowWide},
+  {id:3, title:"10x", subtitle:"Real-time Analytics", icon:BarChart3},
+
+];
 
 
 export default function Home () {
   return (
     <div className='min-h-screen bg-gradient-to-br from-purple-50 to-purple-100 flex items-center justify-center'>
-      <div className="max-w-7xl mx-auto px-4 grid lg:grid-cols-2 gap-12 items-center">
+      <div className="max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
       <div className='container mx-auto px-4 py-16'>
-        <div className='text-center lg:text-left'>
+        <div className='text-center items-center lg:text-left'>
           <h1 className='inline-block text-4xl lg:text-5xl font-extrabold font-sans bg-gradient-to-r from-purple-900 via-indigo-600 to-purple-400 bg-clip-text text-transparent p-4 mb-8'>
            <span className="">Ed&apos;s</span> Inventory Management
           </h1>
@@ -19,7 +34,7 @@ export default function Home () {
              or  adding new items to your inventory.</span>
           </p>
           
-          <div className='flex gap-6 justify-center lg:justify-start'>
+          <div className='mt-22 flex gap-6 justify-center lg:justify-start'>
             <Link 
             href="/sign-in"
             className= " bg-purple-600 text-white px-8 py-2 rounded-lg font-semibold hover:bg-purple-800 shadow-xl transition-colors "
@@ -36,15 +51,38 @@ export default function Home () {
         </div>
       </div>
 
-      <div className="container mx-auto px-4">
-        <Image 
-          src="/img-05.png"
-          alt="inv"
-          width={1200}
-          height={800}
-          className="w-full object-cover"
-        />
+      
+      <div className="flex flex-col items-center ">
+        <div className="container mx-auto px-4 py-10">
+          <div className="flex items-center justify-center py-10">
+            <Image 
+              src="/img-05.png"
+              alt="inv"
+              width={1200}
+              height={800}
+              className="w-full object-cover"
+            />
+          </div>
+          
+          <div className="grid grid-cols-2 md:grid-cols-3 items-center  gap-4 mx-auto flex-none justify-center">
+            {header.map(({id, title, subtitle, icon}) =>{
+              const Icon = icon;
+              return(
+                <div key={id} className="flex flex-col items-center bg-purple-100 border border-purple-200 rounded-lg shadow-lg p-3 m-3 text-center hover:scale-105 transform transition-transform">
+                  <div className="flex flex-row items-center gap-4 mb-2">
+                    <Icon className="text-green-400 "/>
+                    <h4 className="font-black text-gray-800">{title}</h4>
+                  </div>
 
+                  <div className="">
+                    <p className="text-xs lg:text-sm font-medium text-gray-500">{subtitle}</p>
+                  </div>
+
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
 
       </div>
