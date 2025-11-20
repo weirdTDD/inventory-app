d16009f66ad6825eaa726c159c6d14082e9c2255
Edward Morhe <edwardmorhe777@gmail.com>
2025-11-19 20:06:55 -0700

    navbar

--- app/learn-more/page.tsx
+++ app/learn-more/page.tsx
@@ -29,7 +29,7 @@ export default function LearnMore () {
               loading='lazy'
             />
           </div>
-          <div className='absolute sm:hidden flex flex-col ml-2 min-w-full'>
+          <div className='absolute sm:hidden flex flex-col ml-2 min-w-full pointer-events-none'>
             <Image
               src="/logox.png"
               alt="logo"
@@ -42,8 +42,9 @@ export default function LearnMore () {
               <h1 className=' text-xl font-semibold bg-gradient-to-r from-fuchsia-500 via-fuchsia-800 to-indigo-700 bg-clip-text mt-10 text-transparent'>Learn More about us</h1>
 
               <Link 
-                href="/"
+                href="/sign-in"
                 className= " mt-10 bg-purple-600 text-base text-white px-4 py-2 rounded-lg font-semibold hover:bg-purple-800 shadow-xl transition-colors "
+               
                 >
                 Sign In
               </Link>
---
+++ components/navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <nav className='shadow-sm fixed w-full z-10'>
+        <div className='w-full'>
+            <div className='flex items-center h-20 w-full'>
+
+            </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default navbar
